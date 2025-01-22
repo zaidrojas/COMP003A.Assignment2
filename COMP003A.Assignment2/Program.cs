109ed1d8a4c4efdc0439f1dc49de789b1381c9fe
zaidrojas <zaidnemesis83@outlook.com>
2025-01-22 14:08:49 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(
+                "********************************\n" +
+                "Welcome to the Discount Calculator!\n" +
+                "********************************\n" +
+                "What is your first name?: ");
+            string userName = Console.ReadLine();
+        }
+    }
+}

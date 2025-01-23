edf66e046122375d3e59b60c698d27ecca20a50b
zaidrojas <zaidnemesis83@outlook.com>
2025-01-22 16:10:07 -0800

    Added New Code

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -4,12 +4,41 @@
     {
         static void Main(string[] args)
         {
+            string userFirstName;
+            string userLastName;
+            int userAge;
+            int itemPrice;
+            bool student;
+
             Console.Write(
-                "********************************\n" +
+                "******************************************\n" +
                 "Welcome to the Discount Calculator!\n" +
-                "********************************\n" +
+                "******************************************\n" +
                 "What is your first name?: ");
-            string userName = Console.ReadLine();
+            userFirstName = Console.ReadLine();
+
+            Console.Write("What is your last name?: ");
+            userLastName = Console.ReadLine();
+
+            Console.Write("How old are you?: ");
+            userAge = int.Parse(Console.ReadLine());
+
+            Console.Write("What is the price of the item you're interested in?: ");
+            itemPrice = int.Parse(Console.ReadLine());
+
+            Console.Write("Are you a student? (true/false): ");
+            student = bool.Parse(Console.ReadLine());
+
+            Console.Write(
+            "\n\n\n" +
+            "******************************************\n" +
+            $"Hello {userFirstName} {userLastName}\n" +
+            $""+
+            $"" +
+            $"" +
+            $"" +
+            $"");
+
         }
     }
 }
